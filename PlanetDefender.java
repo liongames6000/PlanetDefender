93193e6e2d70b1b93dbd5e00cac3455a62eab84f
LionalMagic <floshop.m@gmail.com>
2021-05-13 21:07:49 +0200

    Es Funktioniert

--- PlanetDefender.java
+++
@@ -1,5 +0,0 @@
-public class PlanetDefender{
-    public public static void main(String[] args) {
-        System.out.println("lmao")
-    }
-}
--- src/main/java/PlanetDefender.java
+++ src/main/java/PlanetDefender.java
@@ -1,2 +1,5 @@
-public class PlanetDefender {
+class PlanetDefender {
+    public static void main(String[] args){
+        System.out.println("hi");
+    }
 }
